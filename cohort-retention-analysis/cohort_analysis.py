8f51b7edfe0f94e9afbe862c23785f80cdd3b722
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-03-24 23:33:58 +0530

    Update cohort_analysis.py

--- cohort-retention-analysis/cohort_analysis.py
+++ cohort-retention-analysis/cohort_analysis.py
@@ -3,7 +3,13 @@ import pandas as pd
 # Load data
 df = pd.read_csv("raw.csv")
 
-# Convert to datetime
+# Convert datetime
 df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
 
-print("Data Loaded:", df.shape)
+# Extract month
+df['order_month'] = df['order_purchase_timestamp'].dt.to_period('M')
+
+# Get cohort (first purchase month)
+df['cohort_month'] = df.groupby('customer_id')['order_month'].transform('min')
+
+print(df[['customer_id', 'order_month', 'cohort_month']].head())
