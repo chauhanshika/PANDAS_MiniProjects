64e31098549d6d69f080018584b38533121a2479
Anshika Chauhan <chauhanshikaa1@gmail.com>
2026-03-25 23:20:20 +0530

    Update cohort_analysis.py

--- cohort-retention-analysis/cohort_analysis.py
+++ cohort-retention-analysis/cohort_analysis.py
@@ -12,7 +12,19 @@ df['order_month'] = df['order_purchase_timestamp'].dt.to_period('M')
 # Assign cohort
 df['cohort_month'] = df.groupby('customer_id')['order_month'].transform('min')
 
-# Cohort index (months since first purchase)
+# Cohort index
 df['cohort_index'] = (df['order_month'] - df['cohort_month']).apply(lambda x: x.n)
 
-print(df[['customer_id', 'cohort_month', 'order_month', 'cohort_index']].head())
+# Group users
+cohort_data = df.groupby(['cohort_month', 'cohort_index'])['customer_id'].nunique().reset_index()
+
+# Pivot
+retention_matrix = cohort_data.pivot(index='cohort_month',
+                                     columns='cohort_index',
+                                     values='customer_id')
+
+# Convert to percentage
+retention_matrix = retention_matrix.divide(retention_matrix[0], axis=0)
+
+print("\nRetention Percentage:")
+print(retention_matrix.head())
